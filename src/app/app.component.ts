595a8040fdcc60c721a78f1408203fa6d8fb878a
cesarluilly <cesarluilly.garcia@hotmail.com>
2022-12-08 08:52:40 -0600

    Video 69 Galerias de trabajos parte 3, nota el codigo js se cambio a forma de como se haria con typescript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'UDY-BootstrapPortafolio';
+
+  constructor(){
+    console.log('Hola mundo constructor');    
+
+    //  CODIGO DEL CURSO DE UDEMY
+    // const imagenes = document.querySelectorAll('.galeria .contenedor-imagen');
+    // const imagenModal = document.getElementById('imagen-modal') as HTMLImageElement;
+
+
+    // imagenes.forEach((img) => {
+    //   img.addEventListener('click', () => {
+    //     const ruta:string = img.querySelector('img')?.src || "";
+    //     imagenModal.src = ruta;
+    //   })
+    // });
+  }
+
+  public cambiarSrc(ruta:string) : void {
+    const imagenModal = document.getElementById('imagen-modal') as HTMLImageElement;
+    imagenModal.src = ruta;
+  }
 }
